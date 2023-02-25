3aa5995f05ef6d6a856e01614809abddf0c8ffdc
Bret Ellis <bmcanelly@gmail.com>
2023-02-25 14:59:55 -0600

    022023 updates

--- .vars.zsh
+++ .vars.zsh
@@ -3,6 +3,8 @@
 if [[ $(uname) == 'Darwin' ]] ; then
   alias vi=/opt/homebrew/bin/vim
   alias vim=/opt/homebrew/bin/vim
+else
+  alias awslogs=$HOME/.local/bin/awslogs
 fi
 
 alias _aws='docker run --rm -it -v ~/.aws:/root/.aws -e LC_ALL=en_US.utf8  -e AWS_PROFILE=$AWS_PROFILE amazon/aws-cli '
@@ -12,19 +14,15 @@ alias xbel='export AWS_PROFILE=bel'
 alias xbwpng='export AWS_PROFILE=bwpng'
 alias xbwsbx='export AWS_PROFILE=bwsbx'
 
-alias devcg='rsp 3000'
-alias devds='be guard'
+alias devcg='rsp 3001'
+alias devds='rsp 4000'
 alias devui='yin ; fmns -f Procfile.dev'
 alias RES='RAILS_ENV=staging'
 
 alias fdec='openssl enc -pbkdf2 -aes256 -d -a -in ${file_name}.enc -out ${file_name}'
 alias fenc='openssl enc -pbkdf2 -aes256 -salt -e -a -in ${file_name} -out ${file_name}.enc'
 
-if [[ $(uname) != 'Darwin' ]] ; then
-  alias awslogs=$HOME/.local/bin/awslogs
-fi
-
-alias lsatr='ls -lAhtr'
+alias lsatr='ls -lAhFtr'
 
 
 
